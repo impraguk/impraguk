46b6b46df9a25dae286f011a3e7266631a22b223
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-24 00:26:40 +0200

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -3,15 +3,8 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-
 @bot.command()
-async def thromka(ctx):
-    await ctx.send("Nub blahin', get back ta' work")
+async def ug(ctx):
+    await ctx.send("Nub blahin', get back ta' work!")
 
 bot.run('NDI5MzE1NDA4NTA4NjE2NzA1.DmDCQQ.qgFaBYsOSlm-Uc5UnEc5yZ_Zd40')
