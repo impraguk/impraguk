a4b72b198d89b0a8974e7cfb2d9663f7c90a21e5
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-24 00:29:11 +0200

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -6,5 +6,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def ug(ctx):
     await ctx.send("Nub blahin', get back ta' work!")
+    
+bot.add_command(test)
 
 bot.run('NDI5MzE1NDA4NTA4NjE2NzA1.DmDCQQ.qgFaBYsOSlm-Uc5UnEc5yZ_Zd40')
