984df9fc4f8ec2af4b671e8d6d1acc340cb5a5c1
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-24 15:47:18 +0200

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -30,7 +30,7 @@ async def on_message(message):
         orkname = random.choice(lines)
         msg = (":speech_balloon:{0.author.mention}**, lat Raguk name iz:** ".format(message) + orkname)
         await client.send_message(message.channel, msg)
-     elif message.content.startswith("!ologname"):
+    elif message.content.startswith("!ologname"):
         ologname = consonants[a] + vowels[b] + consonants[a].lower
         msg = (":speech_balloon:{0.author.mention}**, lat Raguk name iz:** ".format(message) + ologname)
         await client.send_message(message.channel, msg)
