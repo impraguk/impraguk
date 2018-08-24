7067e4913712c65aaf43c539fbe7b0254287092c
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-24 17:38:24 +0200

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -36,7 +36,7 @@ async def on_message(message):
         msg = (":speech_balloon:{0.author.mention}**, lat Raguk name iz:** ".format(message) + ologname)
         await client.send_message(message.channel, msg)
    
-@client.command
+@client.event
 async def help(message):
     if message.content.startswith("!help"):
         embed = discord.Embed(title="Elder Imp'Raguk", description="A notable lore character.", color=0xcc0909)
