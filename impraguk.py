ea4b767f160596a74da85c3d44a005cc1f8f6044
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-24 16:26:21 +0200

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -27,12 +27,13 @@ async def on_message(message):
         await client.send_message(message.channel, msg)
     elif message.content.startswith("!ologname"):
         lines = open('consonants.txt').read().splitlines()
-        ologLetter1 = random.choice(lines)
+        a = random.choice(lines)
         lines = open('vowels.txt').read().splitlines()
-        ologLetter2 = random.choice(lines)
+        b = random.choice(lines)
         lines = open('consonants.txt').read().splitlines()
-        ologLetter3 = random.choice(lines).lower
-        msg = (":speech_balloon:{0.author.mention}**, lat Raguk name iz:** ".format(message) + (ologLetter1.str) + (ologLetter2.str) + (ologLetter3.str))
+        c = random.choice(lines)
+        ologname = a + b + c
+        msg = (":speech_balloon:{0.author.mention}**, lat Raguk name iz:** ".format(message) + ologname)
         await client.send_message(message.channel, msg)
                        
 client.run(os.getenv('TOKEN'))
