bb48b37f61b837545c24a105fc22ed416706c380
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-24 13:13:58 +0200

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -5,14 +5,8 @@ from discord.voice_client import VoiceClient
 import asyncio
 import os
 
-startup_extensions = ["Music"]
 Client = discord.Client()
 client = commands.Bot(command_prefix = "!")
-
-
-class Main_Command():
-        def __init__(self, bot):
-        self.bot = bot
 
 @client.event
 async def on_ready():
@@ -24,13 +18,5 @@ async def on_message(message):
     if message.content.startswith("!ug"):
         msg = ("Nub time for blahin {0.author.mention}, get back ta' working.".format(message))
         await client.send_message(message.channel, msg)
-        
-if __name__ == "__main__":
-    for extension in startup_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = ("(): ()".format(type(e).__name__, e))
-            print ("Failed to load extension".format(extension, exc))
- 
+               
 client.run(os.getenv('TOKEN'))
