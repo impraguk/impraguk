e2790f94dfa8188b9cf3dcd5c6e437a1ccd146be
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-24 15:07:54 +0200

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -22,7 +22,7 @@ async def on_message(message):
     elif message.content.startswith("!name"):
         lines = open('orknames.txt').read().splitlines()
         orkname = random.choice(lines)
-        msg = (":exclamation: {0.author.mention}**, lat Raguk name iz:** ".format(message) + orkname)
+        msg = (":speech_balloon:{0.author.mention}**, lat Raguk name iz:** ".format(message) + orkname)
         await client.send_message(message.channel, msg)
                
                
