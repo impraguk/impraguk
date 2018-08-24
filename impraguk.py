5fc0b5f1d7d41bd7ca906720ad680d4c5e586fe7
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-24 15:45:39 +0200

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -6,8 +6,14 @@ from discord.voice_client import VoiceClient
 import asyncio
 import os
 
+
 Client = discord.Client()
 client = commands.Bot(command_prefix = "!")
+
+consonants = ['B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','X','Z','W','Y']
+a = random.randint(0,21)
+vowels = ['A','E','I','O','U']
+b = random.randint(0,5)
 
 @client.event
 async def on_ready():
@@ -19,11 +25,14 @@ async def on_message(message):
     if message.content.startswith("!ug"):
         msg = ("Nub time for blahin {0.author.mention}, get back ta' working.".format(message))
         await client.send_message(message.channel, msg)
-    elif message.content.startswith("!name"):
+    elif message.content.startswith("!orkname"):
         lines = open('orknames.txt').read().splitlines()
         orkname = random.choice(lines)
         msg = (":speech_balloon:{0.author.mention}**, lat Raguk name iz:** ".format(message) + orkname)
         await client.send_message(message.channel, msg)
-               
-               
+     elif message.content.startswith("!ologname"):
+        ologname = consonants[a] + vowels[b] + consonants[a].lower
+        msg = (":speech_balloon:{0.author.mention}**, lat Raguk name iz:** ".format(message) + ologname)
+        await client.send_message(message.channel, msg)
+                       
 client.run(os.getenv('TOKEN'))
