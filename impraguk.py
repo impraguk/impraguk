d757b360c589931b614ba0f97dfd25198a59fdb9
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-26 21:13:50 +0100

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -59,7 +59,7 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
         
     elif message.content.startswith("!skin"):
-        if "Grunt" in [y.name.lower() for y in author.roles]:  
+        if "Grunt" in [y.name.lower() for y in member.roles]:  
             await client.send_message(message.channel, "{0.author.mention} https://bit.ly/2PCWbjf".format(message))
         
         
