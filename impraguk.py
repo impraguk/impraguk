3d9888c494ad4a39bb6e35111a1be3a0c1b6659d
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-26 20:49:14 +0100

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -59,7 +59,7 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
         
     elif message.content.startswith("!skin"):
-        if message.author.has_any_role("Grunt"):   
+        if author.has_role("Grunt"):   
             await client.send_message(message.channel, "{0.author.mention} https://bit.ly/2PCWbjf".format(message))
         
         
