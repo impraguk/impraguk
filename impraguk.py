7afa8839360885f9b60964bd707dec94ba92dce9
impraguk <42656930+impraguk@users.noreply.github.com>
2018-08-30 16:10:51 +0100

    Update impraguk.py

--- impraguk.py
+++ impraguk.py
@@ -79,6 +79,11 @@ async def on_message(message):
         await client.send_message(message.channel, msg)
         
         
+@client.command(pass_context = True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+        
 @client.event
 async def on_reaction_add(reaction, user):
     channel = reaction.message.channel
